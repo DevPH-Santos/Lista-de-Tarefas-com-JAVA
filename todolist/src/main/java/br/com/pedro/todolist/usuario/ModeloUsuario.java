c7e2bcf5f98cc340242d89645f0bfc14287dc659
DevPH-Santos <pedrohenriquerod.santos@gmail.com>
2025-06-23 16:42:33 -0300

    criando um modelo de usuario e adicionando informações para ele

---
+++ todolist/src/main/java/br/com/pedro/todolist/usuario/ModeloUsuario.java
@@ -0,0 +1,9 @@
+package br.com.pedro.todolist.usuario;
+
+public class ModeloUsuario {
+    
+    private String username;
+    private String nome;
+    private String senha;
+
+}
--- todolist/src/main/java/br/com/pedro/todolist/usuario/UsuarioController.java
+++ todolist/src/main/java/br/com/pedro/todolist/usuario/UsuarioController.java
@@ -1,5 +1,17 @@
 package br.com.pedro.todolist.usuario;
 
+import org.springframework.web.bind.annotation.PostMapping;
+import org.springframework.web.bind.annotation.RequestBody;
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RestController;
+
+@RestController
+@RequestMapping("/usuarios")
 public class UsuarioController {
     
+    @PostMapping("/")
+    public void create(@RequestBody ModeloUsuario modeloUsuario){//vai ta pegando as informações do corpo da requisição, tipo o POST do PHP
+        System.out.println(modeloUsuario.username);
+    }
+
 }
