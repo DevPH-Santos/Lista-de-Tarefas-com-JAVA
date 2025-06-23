7f646938b4be5c25f7f75ee6cb2b2be3c68f8119
DevPH-Santos <pedrohenriquerod.santos@gmail.com>
2025-06-23 17:13:45 -0300

    instalando a biblioteca Lombok para substituir os codigos fisicos de getters e setters dos atributos

--- todolist/src/main/java/br/com/pedro/todolist/usuario/ModeloUsuario.java
+++ todolist/src/main/java/br/com/pedro/todolist/usuario/ModeloUsuario.java
@@ -1,42 +1,12 @@
 package br.com.pedro.todolist.usuario;
 
+import lombok.Data;
+
+@Data//quando se usa essa biblioteca não precisa colocar mais os getters e setters
 public class ModeloUsuario {
     
     private String username;
     private String nome;
     private String senha;
-
-    //setters
-    //username
-    public void setUsername(String username) {
-        this.username = username;
-    }
-
-    //nome
-    public void setNome(String nome) {
-        this.nome = nome;
-    }
-
-    //senha
-    public void setSenha(String senha) {
-        this.senha = senha;
-    }
-
-
-    //getters
-    //username
-    public String getUsername() {
-        return username;
-    }
-
-    //nome
-    public String getNome() {
-        return nome;
-    }
-
-    //senha
-    public String getSenha() {
-        return senha;
-    }
 
 }
